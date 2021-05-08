f63cba3510f18a6c12b1fe51e640055dd592e883
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-08 20:02:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {BrowserRouter as Router , Switch , Route } from "react-router-dom"
+import Login from "./components/Login"
+
 
 function App() {
   return (
     <div className="App">
-     <h1>LinkedIn clone</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login/>
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
