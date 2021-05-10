9f51ce81ed82044870994f80e40b2b7af50c63ea
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-10 23:51:29 +0530

    commit

--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -1,15 +1,41 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-const Rightside =(props) =>{
-    return(
-        <Container>Right side</Container>
-    )
-}
+const Rightside = (props) => {
+  return (
+    <Container>
+      <FollowCard>
+        <Title>
+          <h2>Add to your feed</h2>
+          <img src="/images/feed-icon.svg" alt="" />
+        </Title>
+      </FollowCard>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-    grid-area:Rightside;
+  grid-area: Rightside;
 `;
 
+const FollowCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 /15%), 0 0 0 rgb(0 0 0/20%);
+  padding: 12px;
+`;
 
+const Title = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  width: 100%;
+  color: rgba(0, 0, 0, 0.6);
+`;
 
 export default Rightside;
