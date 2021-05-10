8b9ca2f76a9296e619b1181d07524c0f557525ba
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-10 23:52:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import './App.css';
-import {BrowserRouter as Router , Switch , Route } from "react-router-dom"
-import Login from "./components/Login"
-import Home from './components/Home';
-import Header from './components/Header';
-
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Login/>
-        </Route>
-        <Route path="/home">
-          <Header/>
-          <Home/>
-        </Route>
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
