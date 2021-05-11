4da000143f37a8a059c72c57843170b1909f7d7a
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-11 22:54:05 +0530

    commit

--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -8,6 +8,13 @@ const Rightside = (props) => {
           <h2>Add to your feed</h2>
           <img src="/images/feed-icon.svg" alt="" />
         </Title>
+        <FeedList>
+            <li>
+                <a>
+                    <Avatar/>
+                </a>
+            </li>
+        </FeedList>
       </FollowCard>
     </Container>
   );
