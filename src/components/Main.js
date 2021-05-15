304f64401c0aa18db8253729f942f99fabe0e0af
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-15 13:00:54 +0530

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,36 @@
 import styled from "styled-components";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import PostModal from "./PostModal";
-
+import { useState } from "react";
 
 const Main = (props) => {
+  const [showModal, setShowModal] = useState("close");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (e.target !== e.currentTarget) {
+      return;
+    }
+
+    switch (showModal) {
+      case "open":
+        setShowModal("close");
+        break;
+      case "close":
+        setShowModal("open");
+        break;
+      default:
+        setShowModal("close");
+        break;
+    }
+  };
+
   return (
     <Container>
       <ShareBox>
         <div>
           <img src="/images/user.svg" alt="" />
-          <button>Start a post</button>
+          <button onClick={handleClick}>Start a post</button>
         </div>
         <div>
           <button>
@@ -91,7 +112,7 @@ const Main = (props) => {
           </SocialActions>
         </Article>
       </div>
-      <PostModal/>
+      <PostModal showModal={showModal} handleClick={handleClick}/>
     </Container>
   );
 };
--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,75 +1,252 @@
-import styled from "styled-components"
-import CloseIcon from '@material-ui/icons/Close';
+import styled from "styled-components";
+import CloseIcon from "@material-ui/icons/Close";
+import ImageIcon from "@material-ui/icons/Image";
+import ScreenShareIcon from "@material-ui/icons/ScreenShare";
+import { IconButton } from "@material-ui/core";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import { useState } from "react";
 
-const PostModal =(props) =>{
-    return(
+const PostModal = (props) => {
+  const [editorText, setEditorText] = useState("");
+
+  const [shareImage, setShareImage] = useState("");
+
+  const handleChange = (e) => {
+    const image = e.target.files[0];
+
+    if (image === "" || image === undefined) {
+      alert(`not an image, the file is a ${typeof image}`);
+      return;
+    }
+    setShareImage(image);
+  };
+
+  const reset = (e) => {
+    setEditorText("");
+    props.handleClick(e);
+  };
+
+  return (
+    <>
+      {props.showModal === "open" && (
         <Container>
-            <Content>
-                <Header>
-                    <h2>Create a post</h2>
-                    <button>
-                        <CloseIcon />
-                    </button>
-                </Header>
-            </Content>
+          <Content>
+            <Header>
+              <h2>Create a post</h2>
+              <button onClick={(event) => reset(event)}>
+                <CloseIcon style={{ color: "gray" }} />
+              </button>
+            </Header>
+            <SharedContent>
+              <UserInfo>
+                <img src="/images/user.svg" alt="" />
+                <span>Name</span>
+              </UserInfo>
+
+              <Editor>
+                <textarea
+                  value={editorText}
+                  onChange={(e) => setEditorText(e.target.value)}
+                  placeholder="what do you want to talk about?"
+                  autoFocus={true}
+                />
+                <UploadImage>
+                  <input
+                    type="file"
+                    accept="image/gif, image/jpeg, image/png"
+                    name="image"
+                    id="file"
+                    style={{ display: "none" }}
+                    onChange={handleChange}
+                  />
+                  <p>
+                    <label htmlFor="file" style={{ cursor: "pointer" }}>
+                      Select an image to share
+                    </label>
+                  </p>
+                  {shareImage && <img src={URL.createObjectURL(shareImage)} />}
+                </UploadImage>
+              </Editor>
+            </SharedContent>
+            <SharedCreation>
+              <AttachAssets>
+                <AssetButton>
+                  <IconButton>
+                    <ImageIcon />
+                  </IconButton>
+                </AssetButton>
+                <AssetButton>
+                  <IconButton>
+                    <ScreenShareIcon />
+                  </IconButton>
+                </AssetButton>
+              </AttachAssets>
+              <ShareComment>
+                <AssetButton>
+                  <IconButton>
+                    <InsertCommentIcon />
+                  </IconButton>
+                  Anyone
+                </AssetButton>
+              </ShareComment>
+              <PostButton disabled={!editorText ? true : false}>
+                Post
+              </PostButton>
+            </SharedCreation>
+          </Content>
         </Container>
-    )
-}
-
-
-
+      )}
+    </>
+  );
+};
 
 const Container = styled.div`
-    position:fixed;
-    top:0;
-    left:0;
-    right:0;
-    bottom:0;
-    z-index:9999;
-    color:black;
-    background-color:rgba(0,0,0,0.8);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 9999;
+  color: black;
+  background-color: rgba(0, 0, 0, 0.8);
+  animation: fadeIn 0.3s;
 `;
 
 const Content = styled.div`
-    width:100%;
-    max-width:552px;
-    background-color:white;
-    max-height:90%;
-    overflow:initial;
-    border-radius:5px;
-    position: relative;
-    display:flex;
-    flex-direction:column;
-    top:32px;
-    margin:0 auto;
-
+  width: 100%;
+  max-width: 552px;
+  background-color: white;
+  max-height: 90%;
+  overflow: initial;
+  border-radius: 5px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  top: 32px;
+  margin: 0 auto;
 `;
 
 const Header = styled.div`
-    display:block;
-    padding:16px 20px;
-    border-bottom:1px solid rgba(0,0,0,0.15);
-    font-size:16px;
-    line-height:1.5;
-    color:rgba(0,0,0,0.6);
-    font-weight:100;
-    display:flex;
-    justify-content:space-between;
-    align-items:center;
+  display: block;
+  padding: 16px 20px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.6);
+  font-weight: 100;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-    button{
-        height:40px;
-        width:40px;
-        min-width:auto;
-        color:rgba(0,0,0,0.15);
-            
-            svg{
-                pointer-events:none;
-            }
-       
+  button {
+    height: 40px;
+    width: 40px;
+    min-width: auto;
+    color: rgba(0, 0, 0, 0.15);
+
+    svg {
+      pointer-events: none;
     }
-
+  }
 `;
 
+const SharedContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow-y: auto;
+  vertical-align: baseline;
+  background: transparent;
+  padding: 8px 12px;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 24px;
+
+  svg,
+  img {
+    width: 48px;
+    height: 48px;
+    background-clip: content-box;
+    border: 2px solid transparent;
+    border-radius: 50%;
+  }
+
+  span {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1.5;
+    margin-left: 5px;
+  }
+`;
+
+const SharedCreation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 24px 12px 16px;
+`;
+
+const AssetButton = styled.div`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  min-width: auto;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const AttachAssets = styled.div`
+  align-items: center;
+  display: flex;
+  padding-right: 8px;
+
+  ${AssetButton} {
+    width: 40px;
+  }
+`;
+
+const ShareComment = styled.div`
+  padding-left: 8px;
+  margin-right: auto;
+  border-left: 1px solid rgba(0, 0, 0, 0.15);
+  ${AssetButton} {
+    svg {
+      //margin-right: 5px;
+    }
+  }
+`;
+
+const PostButton = styled.button`
+  min-width: 60px;
+  border-radius: 20px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background: ${(props) => (props.disabled ? "rgba(0,0,0,0.8)" : "#0a66c2")};
+  color: ${(props) => (props.disabled ? "rgba(1,1,1,0.2)" : "white")};
+
+  &:hover {
+    background: ${(props) => (props.disabled ? "rgba(0,0,0,0.08)" : "#004182")};
+  }
+`;
+
+const Editor = styled.div`
+  padding: 12px 24px;
+  textarea {
+    width: 100%;
+    min-height: 100px;
+    resize: none;
+    border: none;
+  }
+
+  input {
+    width: 100%;
+    height: 35px;
+    font-size: 16px;
+    margin-bottom: 20px;
+  }
+`;
+
+const UploadImage = styled.div``;
 
 export default PostModal;
